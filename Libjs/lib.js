ca7a620505dea2dd0c641f6ba2969a803d4b917e
Timothee Behra <timotheebehra.etu@gmail.com>
2019-10-06 18:38:46 +0200

    libjs

---
+++ Libjs/lib.js
@@ -0,0 +1,42 @@
+var select = (function() {
+
+	function addClass(newClass) {
+		for (var i = 0; i < this.length; i++) {
+			this[i].className += ' ' + newClass;
+		}
+		return this;
+	}
+
+	function removeClass(classToRemove) {
+		for (var i = 0; i < this.length; i++) {
+			var oldClasses = this[i].className.split(' ');
+			var newClasses = [];
+
+			for (var j = 0; j < oldClasses.length; j++) {
+				if (oldClasses[j] !== classToRemove) {
+					newClasses.push(oldClasses[j]);
+				}
+			}
+			this[i].className = newClasses.join(' ');
+		}
+		return this;
+	}
+
+	function changeText(newContent) {
+		for (var i = 0; i < this.length; i++) {
+			this[i].textContent = newContent;
+		}
+		return this;
+	}
+
+	function select(str) {
+		var selectedElems = document.querySelectorAll(str);
+		selectedElems.addClass = addClass;
+		selectedElems.removeClass = removeClass;
+		selectedElems.changeText = changeText;
+
+		return (selectedElems);
+	}
+
+	return select;
+})();
